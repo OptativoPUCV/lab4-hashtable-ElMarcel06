8865ab504fcd2c1e5d9a0db546756916cdf9f6a1
ElMarcel06 <163350482+ElMarcel06@users.noreply.github.com>
2024-04-24 21:54:27 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -67,12 +67,12 @@ void insertMap(HashMap * map, char * key, void * value) {
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
     Pair ** aux = map->buckets;
-    map->capacity = map->capacity*2+1;
+    map->capacity = map->capacity*2;
     map->buckets = (Pair **)calloc(map->capacity,sizeof(Pair *));
     map->size=0;
     for(int i=0;i<map->capacity;i++){
       if(aux[i]!=NULL){
-        
+        insertMap(map,aux[i]->key,aux[i]->value);
         
       }
       else{
