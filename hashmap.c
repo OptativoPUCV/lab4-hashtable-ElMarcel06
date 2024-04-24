b3ff2779cca827adbdd8ee4079a32fd7031b7941
ElMarcel06 <163350482+ElMarcel06@users.noreply.github.com>
2024-04-24 22:14:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -152,6 +152,7 @@ Pair * firstMap(HashMap * map) {
 
 Pair * nextMap(HashMap * map) {
   if(map==NULL) return NULL;
+  if(map->current==-1) return NULL;
   for(int i = 0; i<map->capacity;i++){
     if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL){
       map->current=i;
